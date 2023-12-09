74214cf5f87bf9893d80b33d5dc2dbc9d81ea91d
Your Name <you@example.com>
2023-12-09 14:43:47 +0900

    code freeze

--- app.py
+++ app.py
@@ -9,6 +9,60 @@ import aiohttp
 url = "https://deliverysupport.stg.pioneerapis.com/"
 api_key = "PAK1 Credential=zPsJKOJCxNbkpddLbLoZ Signature=6a75i625tzfqok3mchxe79831o2t5u0gccl9np0eyf3mq61e29megt8ml3f2htky"
 
+depo = {
+      "lat": 35.007992,
+      "lon": 135.775486,
+    }
+
+kiyomizu = {
+        "lat": 34.9946662,
+        "lon": 135.7820861
+      }
+
+yasaka = {
+        "lat": 35.0036559,
+        "lon": 135.760529
+      }
+
+ponto = {
+        "lat": 35.0042777,
+        "lon": 135.7685495
+      }
+
+
+def get_json_route(start, dest):
+    return {
+        "userID": "testUser",
+        "pointinfo": {
+            "start": start,
+            "destination": dest,
+        },
+        "setTime": {
+            "type": 0,
+            "time": "2023-01-01T08:30+09:00"
+        },
+        "options": {
+            "priority": "recommended",
+            "useToll": True,
+            "useHighway": True,
+            "useFerry": True,
+            "useSmartIC": True,
+            "careJam": True,
+            "careRegulation": True,
+            "careTimeRegulation": True
+        },
+        "vehicleInfo": {
+            "type": 0,
+            "ratesClass": 0,
+            "etc": 0,
+            "width": 178,
+            "height": 153,
+            "weight": 1480,
+            "dangerousLoaded": False
+        },
+        "needDrawData": True
+        }
+
 json_data = {
   "userID": "testUser",
   "pointinfo": {
@@ -57,6 +111,54 @@ json_data = {
   "needDrawData": True
 }
 
+json_routing = {
+  "map_region": "jp",
+  "user_id": "testUser",
+  "delivery_start_time": "2020-01-01T14:30+09:00",
+  "delivery_end_time": "2020-01-01T18:30+09:00",
+  "depot": {
+    "unique_id": "depot",
+    "address": {
+      "latitude": 35.007992,
+      "longitude": 135.775486,
+    }
+  },
+  "locations": [
+    {
+      "unique_id": "kiyomizu",
+      "address": {
+        "latitude": 34.9946662,
+        "longitude": 135.7820861
+      },
+    },
+    {
+      "unique_id": "yasaka",
+      "address": {
+        "latitude": 35.0036559,
+        "longitude": 135.760529
+      },
+    },
+    {
+      "unique_id": "pontocho",
+      "address": {
+        "latitude": 35.0042777,
+        "longitude": 135.7685495
+      },
+    },
+  ],
+  "search_condition": {
+    "use_care_traffic": False,
+    "use_care_regulation": False,
+    "use_care_time_regulation": True,
+    "use_alongside": False,
+    "use_toll": False,
+    "use_hwy": False,
+    "use_ferry": False,
+    "use_smart_ic": False
+  },
+  "need_draw_data": True
+}
+
 app = Flask(__name__)
 CORS(app)
 
@@ -78,7 +180,7 @@ def sample():
         return redirect(url_for("test", args = keyword))
     return render_template("sample.html")
 
-async def make_async_request():
+async def make_async_request(target, json):
     async with aiohttp.ClientSession() as session:
         headers = {
             'Authorization': api_key,
@@ -86,7 +188,7 @@ async def make_async_request():
             'PEC-Traffic-ProviderKey': 'none',
             'PEC-Traffic-ProviderUserID': "testUser",
         }
-        async with session.post(url + "/navicore/calcRoute", headers=headers, json=json_data) as response:
+        async with session.post(url + target, headers=headers, json=json) as response:
             return await response.json()
 
 @app.route('/api', methods=['GET'])
@@ -94,8 +196,34 @@ def api():
     try:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-        response = loop.run_until_complete(make_async_request())
-        print('pass')
+        response = loop.run_until_complete(make_async_request("/navicore/calcRoute", get_json_route(yasaka, kiyomizu)))
+        return jsonify(response)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/hidden', methods=['GET'])
+def get_routing_all():
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        order = {"depo_ponto.json":(depo, ponto), "ponto_yasaka.json":(ponto, yasaka), "yasaka_kiyomzu.json":(yasaka, kiyomizu)}
+        for i, (key, (start, dest)) in enumerate(order.items()):
+            if i!=2:
+                continue
+            print(key)
+            response = loop.run_until_complete(make_async_request("/navicore/calcRoute", get_json_route(depo, ponto)))
+            with open("data/"+key, 'w') as f:
+                json.dump(response, f)
+        return "success"
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/vehicle', methods=['GET'])
+def api_vehicle():
+    try:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        response = loop.run_until_complete(make_async_request("/caerus/vehiclerouting/v1/order", json_routing))
         return jsonify(response)
     except Exception as e:
         return jsonify({'error': str(e)})
